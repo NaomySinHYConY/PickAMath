d706667e76cd206f2d906aa9d703d84d9870d479
DamaBeth <44103927+DamaBeth@users.noreply.github.com>
2021-03-25 06:38:48 -0600

    Elementos

--- src/scenes/Scene_grupos.js
+++ src/scenes/Scene_grupos.js
@@ -6,6 +6,11 @@ class Scene_grupos extends Phaser.Scene {
     preload() {
         console.log('Scene_grupos');
         this.load.setPath('./assets/');
+
+        this.load.image('galaxia','galaxia.png');
+        this.load.image('exit', '/Botones/eliminar.png');
+        this.load.image('logo', 'logo.png');
+        this.load.image('tabla_grupos','tabla_grupos.png');
     }
 
     create() {
