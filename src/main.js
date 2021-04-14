611e1e4c6e487d249efc24280185623b76790358
DamaBeth <44103927+DamaBeth@users.noreply.github.com>
2021-04-14 01:50:16 -0500

    Actualización de versión phaser 3.54.0

--- src/main.js
+++ src/main.js
@@ -9,6 +9,19 @@ import Scene_grupos from './scenes/Scene_grupos.js';
 import Scene_agregarGrupo from './scenes/Scene_agregarGrupo.js';
 import Scene_detallesCat from './scenes/Scene_detallesCat.js';
 
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDaEjBxuqjQWwzYIjh2Uy78UzjHjiZ6LRE",
+    authDomain: "pickamath.firebaseapp.com",
+    databaseURL: "https://pickamath-default-rtdb.firebaseio.com",
+    projectId: "pickamath",
+    storageBucket: "pickamath.appspot.com",
+    messagingSenderId: "728309485467",
+    appId: "1:728309485467:web:d4d15d57d408bbde3b5b3e",
+    measurementId: "G-98W3WXJZG1"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const config = {
     title: "Pick-A-Math",
